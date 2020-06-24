534b4f3f3470a41e27eb212cf8722357ed6000c0
jlopez1328 <58013117+jlopez1328@users.noreply.github.com>
2020-06-24 18:58:54 -0500

    tarea

---
+++ areas.js
@@ -0,0 +1,5 @@
+const m = require('.');
+
+console.log(m.areaTriangulo(1, 2));
+console.log(m.areaCuadrado(2, 24));
+console.log(m.areaCirculo( 6));
---
+++ pruebas.js
@@ -0,0 +1,11 @@
+const m = require('.');
+
+console.log(m.suma(1, 2));
+console.log(m.suma(1234, 2));
+console.log(m.resta(11, 24));
+console.log(m.resta(1, 2));
+console.log(m.multiplicacion(2, 24));
+console.log(m.division(50, 6));
+console.log(m.areaTriangulo(1, 2));
+console.log(m.areaCuadrado(2, 24));
+console.log(m.areaCirculo( 6));
